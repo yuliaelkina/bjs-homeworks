3a1b12258fedd249229957bae080c72479e64e9d
yulia elkina <todg@mail.ru>
2021-01-15 14:46:08 +0300

    hw 1.1 done

--- 1.1-variables/task.js
+++ 1.1-variables/task.js
@@ -7,21 +7,31 @@
 
 function averageMark(a, g, p) {
 	
-    // реализуйте здесь задание №1
+		let algebra = a;
+		let geography = g;
+		let physics = p;
+
+		let average = (a + g + p)/3;
     
-	return // ваш ответ;
+	return average;// ваш ответ;
 }
 
 function sayHello(userName) {
 	
+	let myName = userName;
+	let message = `Привет, мир! Меня зовут ${myName}`;
     // реализуйте здесь задание №2
     
-	return // ваш ответ;
+	return message; // ваш ответ;
 }
 
 function calculateFormula() {
-    
+		let x = 2;
+		let y = 22;
+		let z = 0;
+
+		let result = x * y + 5 * z + x - 1;
 	// реализуйте здесь задание №3
     
-    return // ваш ответ;
+    return result;// ваш ответ;
 }
