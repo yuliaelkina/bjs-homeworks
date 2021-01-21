ad9031eee575cffc3cd541c824ad6d0e57163a02
yulia elkina <todg@mail.ru>
2021-01-21 14:38:38 +0300

    hw 2.2 completely done

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,47 @@
-//String.prototype.isPalindrome - для задачи №1
+String.prototype.isPalindrome = function() {
+  let text =this.toLowerCase();
+  text = text.split(' ').join('');
+let mirrorText = reverseString(text);
+  if (text == mirrorText) {
+      return true;
+  }
+  else {return false;
+};
+};
+
+function reverseString(str) {
+    let reverse = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        reverse += str[i];
+    }
+    return reverse;
+};
+
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
-}
+    let amount = 0;
+  if (marks.length == 0) {
+    return +0;
+  }
+  for (let i = 0; i < marks.length; i++) {
+  amount += marks[i];
+  };
+  let average = amount/marks.length;
+  let roundedAverage = Math.round(average);
+  return roundedAverage; 
+};
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    const now = new Date().getTime();
+    const birthdayDate = new Date(birthday).getTime();
+    let diff = now - birthdayDate;
+    const year = 31557600000;
+    let age = diff/year;
+    if (age >= 18) {
+        return true;
+    }
+    else { 
+        return false;
+    }
 }
+
